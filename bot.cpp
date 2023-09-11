4fd1d5eb6def28864e6da7fffb430d83e7b061a7
Poko-Apps <parimals741@gmail.com>
2023-09-11 18:22:23 +0530

    Initial commit

---
+++ bot.cpp
@@ -0,0 +1,162 @@
+/**
+ * Created by poko on 11-09-2023
+ * Concept and ref python code by Rob Dundas
+*/
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include <Windows.h>
+#include <iostream>
+#include <chrono>
+
+#define THRESHOLD 1 //means if there is less than 1 (basically 0) black pixel in img then catch the fish
+
+
+/**
+ * Returns a 30x30 screenshot around mouse cursor
+*/
+cv::Mat getScreenshot()
+{
+    POINT mousePos;
+    GetCursorPos(&mousePos);
+
+    // box area around the mouse pos
+    int captureWidth = 30;
+    int captureHeight = 30;
+    int X = mousePos.x - (captureWidth/2);
+    int Y = mousePos.y - (captureHeight/2);
+
+    // device ctx for entire screen
+    HDC ScreenDeviceCtx = GetDC(NULL);
+
+    HDC captureDC = CreateCompatibleDC(ScreenDeviceCtx);
+
+    HBITMAP hBitmap = CreateCompatibleBitmap(ScreenDeviceCtx, captureWidth, captureHeight);
+
+    SelectObject(captureDC, hBitmap);
+
+    BitBlt(captureDC, 0, 0, captureWidth, captureHeight, ScreenDeviceCtx, X, Y, SRCCOPY);
+
+    BITMAPINFOHEADER bi;
+    ZeroMemory(&bi, sizeof(bi));
+
+    bi.biSize = sizeof(BITMAPINFOHEADER);
+    bi.biWidth = captureWidth;
+    bi.biHeight = -captureHeight;
+    bi.biPlanes =1;
+    bi.biBitCount = 32; //4 byte cuz RGBA
+    bi.biCompression = BI_RGB;
+    bi.biSizeImage = 0;
+
+    cv::Mat mat = cv::Mat(captureHeight, captureWidth, CV_8UC4);
+    GetDIBits(captureDC, hBitmap, 0, captureHeight, mat.data, (BITMAPINFO*)&bi, DIB_RGB_COLORS);
+
+    DeleteObject(hBitmap);
+    DeleteDC(captureDC);
+    ReleaseDC(NULL, ScreenDeviceCtx);
+
+    return mat;
+}
+
+// Small util function that return sum of black pixels in a Mat (image)
+int sumBlackPixel(cv::Mat mat)
+{
+    return (mat.rows * mat.cols) - cv::countNonZero(mat);
+}
+
+// Perform right click
+void rightClick()
+{
+    INPUT inp = {0};
+    inp.type = INPUT_MOUSE;
+    inp.mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
+    SendInput(1, &inp, sizeof(inp));
+
+    inp.type = INPUT_MOUSE;
+    inp.mi.dwFlags = MOUSEEVENTF_RIGHTUP;
+    SendInput(1, &inp, sizeof(inp));
+}
+
+// I use it for debug
+void calcFPS()
+{
+    static auto startTime = std::chrono::steady_clock::now();
+    static int fps = 0;
+
+    auto currentTime = std::chrono::steady_clock::now();
+    auto elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(currentTime - startTime).count();
+
+    if(elapsedTime >= 1)
+    {
+        startTime = currentTime;
+        std::cout << "FPS: " << fps << std::endl;
+        fps=0;
+    }
+    else{
+        fps++;
+    }
+}
+
+
+int main()
+{
+    //Show some general instrcution
+    std::cout << "[*] Press [6] to On-Off BOT" << std::endl;
+    std::cout << "[*] Press [0] (In-Game) to Exit BOT , or" << std::endl;
+    std::cout << "[*] Press [ESC] to Exit BOT" << std::endl;
+    std::cout << "[+] Note: Pressing cross (X) won't exit BOT" << std::endl;
+
+
+    cv::namedWindow("Output", cv::WINDOW_NORMAL);
+    
+    bool bot = false;
+    int key = -1;
+    bool firstTime = true;
+    uint64_t fish_count = 0;
+
+    while (key != 27 && !GetAsyncKeyState(VK_NUMPAD0))
+    {
+        if(GetAsyncKeyState(VK_NUMPAD6))
+        {
+            bot = !bot;
+            firstTime = true;
+            Sleep(50);
+        }
+        
+        cv::Mat Snapshot = getScreenshot();
+        cv::cvtColor(Snapshot, Snapshot, cv::COLOR_BGR2GRAY);
+        //calcFPS();
+        cv::imshow("Output", Snapshot);
+
+        if(bot)
+        {
+            if(firstTime)
+            {
+                rightClick();//cast the rod for the first time
+                Sleep(1900);
+                firstTime = false;
+                goto label;
+            }
+
+            if(sumBlackPixel(Snapshot) < THRESHOLD )
+            {
+                fish_count++;
+                std::cout << "Fish : "<< fish_count << std::endl;
+
+                rightClick();// to get the fish
+                Sleep(1300);//wait 1.3 seconds
+                rightClick();//cast the rod again
+                Sleep(2000);//wait 2 seconds before processing any further
+            }
+        }
+label:
+        key = cv::waitKey(35);
+    }
+    
+    cv::destroyAllWindows();
+    return 0;
+}
+
+
